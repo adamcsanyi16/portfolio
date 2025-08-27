e4684b49af56021ced8668adbadf8217cea85b04
adamcsanyi16 <csanyiadam8@gmai.com>
2025-08-27 15:57:11 +0200

    routing 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./pages/Navbar";
 import Footer from "./pages/Footer";
